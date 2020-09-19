96ee73c9a7015c580a6f0275673723ddcfb12c10
Kevin Burgon <kevin.burgon@gmail.com>
2020-09-19 09:57:45 -0600

    Add ability to list available sound bytes
    Adds a method to the SoundPlayer class which gives the ability to list
    the sounds that are available to play in the Sounds folder.

--- SoundPlayer.py
+++ SoundPlayer.py
@@ -1,11 +1,15 @@
 from pydub import AudioSegment
 from pydub.playback import play
+from os import listdir
+from os.path import isfile, join
 
 class SoundPlayer(object):
     def __init__(self):
         self.root_path = 'Sounds/'
-    def play(self, sound_name='hag_idle'):
+    def list_sounds(self):
+        return [f for f in listdir(self.root_path) if isfile(join(self.root_path, f))]
+    def play(self, sound_name):
         print('playing name ' + sound_name)
-        full_name = self.root_path + sound_name + '.mp3'
+        full_name = self.root_path + sound_name
         song = AudioSegment.from_mp3(full_name)
         play(song)

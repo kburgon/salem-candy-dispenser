b71089948eddd670b541e54aba076951707d84f4
Kevin Burgon <kevin.burgon@gmail.com>
2020-09-19 10:46:06 -0600

    Add basic logic flow

--- salemCandyDispenser.py
+++ salemCandyDispenser.py
@@ -1,4 +1,27 @@
 from SoundPlayer import SoundPlayer
+import serial
+import time
 
-player = SoundPlayer()
-player.play('hag_idle')
+heartbeat_value = 1
+candy_triggered = 2
+port = '/dev/ttyACM0'
+
+def run():
+    # Initialize Objects
+    arduino = serial.Serial(port, 9600, timeout=5)
+    time.sleep(2)
+
+    # Listen for input
+    arduino.flush()
+    response = arduino.read(arduino.in_waiting())
+
+    # Check input for action
+    response_num = int(response)
+    if response_num == heartbeat_value:
+        print('Arduino Alive')
+    elif response_num == candy_triggered:
+        print('Candy triggered')
+        play_sound()
+
+def play_sound():
+    print('playing sound')

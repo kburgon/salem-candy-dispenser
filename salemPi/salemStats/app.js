cc02e05af111b7a73c3e0887edfe40ee0ef34582
Kevin Burgon <kevin.burgon@gmail.com>
2020-10-17 16:21:01 -0600

    Create working simple js app with views
    Creates a simple js app with routing and a folder for html pages.

---
+++ salemPi/salemStats/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname + '/views' });
+});
+
+app.use('/', router);
+app.listen(process.env.port || port);
+console.log("Running on port ${port}");

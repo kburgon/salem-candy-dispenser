562c1b21c40d1d254eeae8246384b447e9525d1d
Kevin Burgon <kevin.burgon@gmail.com>
2020-10-27 23:03:12 -0600

    Create working chart, create log endpoint
    Creates a working sample instance of Chart.js on the home page and
    creates the logs endpoint which can read from the log database.

--- salemPi/salemStats/app.js
+++ salemPi/salemStats/app.js
@@ -3,11 +3,46 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const router = express.Router();
+const sql = require('sqlite3').verbose();
 
 router.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname + '/views' });
 });
 
+router.get('/logs', (req, res) => {
+    let db = new sql.Database(__dirname + '/DB/log.db', (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+
+        console.log('Connected to Database');
+    });
+    let query = 'SELECT loggedAt FROM trigger_log;';
+    var results = db.all(query, (err, rows) => {
+        if (err) {
+            return console.error(err.message);
+        }
+
+        console.log('Getting results');
+        var res = new Array();
+        rows.forEach((row) => {
+            console.log('Result: ' + row.loggedAt);
+            res.push(row.loggedAt);
+        });
+        return res;
+    });
+    console.log(results);
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+    });
+    res.send({
+        data: results
+    });
+})
+
 app.use('/', router);
+app.use('/logs', router);
 app.listen(process.env.port || port);
 console.log("Running on port " + port);
